70d441a0ad07274a22214ef357f5660411975400
Mayank Kumar <kmayank38@gmail.com>
2023-03-25 18:53:54 +0530

    added dashboard template

--- CampusConnect/urls.py
+++ CampusConnect/urls.py
@@ -21,7 +21,9 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('app/', include('CampusConnectApp.urls')),
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="login.html")),
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
+    path('dashboard', TemplateView.as_view(
+        template_name="dashboard.html"), name="dashboard")
 ]
